78f99d069fa2da9c485f8c483c6bd994c19c0fe2
tomasdelak <tdelallave@itba.edu.ar>
2025-05-26 14:49:09 -0300

    Delete 13_InfoMed_TP5_DelaLlaveErbin.sql

--- 13_InfoMed_TP5_DelaLlaveErbin.sql
+++
@@ -1,7 +0,0 @@
--- Actividad 13.
-
-SELECT COUNT(*) AS total_recetas, p.id_paciente AS id_paciente, p.nombre
-FROM recetas r
-JOIN pacientes p ON r.id_paciente = p.id_paciente
-GROUP BY p.id_paciente, p.nombre
-ORDER BY total_recetas DESC;

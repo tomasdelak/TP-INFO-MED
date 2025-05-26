1fd2f4b8b75ec86100d38b7fc7c9d30f0093cec7
LolaErbin <lerbin@itba.edu.ar>
2025-05-26 13:35:15 -0300

    Cargo commit 13

---
+++ 13_InfoMed_TP5_DelaLlaveErbin.sql
@@ -0,0 +1,7 @@
+-- Actividad 13.
+
+SELECT COUNT(*) AS total_recetas, p.id_paciente AS id_paciente, p.nombre
+FROM recetas r
+JOIN pacientes p ON r.id_paciente = p.id_paciente
+GROUP BY p.id_paciente, p.nombre
+ORDER BY total_recetas DESC;

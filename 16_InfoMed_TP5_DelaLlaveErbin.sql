30d862e22bc86b92ee19b0d53bbfce99cf8aeaf1
tomasdelak <tdelallave@itba.edu.ar>
2025-05-26 20:42:15 -0300

    query 16

--- 16_InfoMed_TP5_DelaLlaveErbin.sql
+++ 16_InfoMed_TP5_DelaLlaveErbin.sql
@@ -1,11 +1,11 @@
 SELECT 
-  m.nombre as nombre_medico,
-  p.nombre as nombre_paciente,
-  count(*) as total_consultas
- 
- from consultas c
- join medicos m on c.id_medico = m.id_medico
- join pacientes p on c.id_paciente = p.id_paciente
- group by m.nombre, p.nombre
- order by m.nombre;
+  m.nombre AS nombre_medico,
+  p.nombre AS nombre_paciente,
+  COUNT(c.id_consulta) AS total_consultas
+FROM medicos m
+LEFT JOIN consultas c ON m.id_medico = c.id_medico
+LEFT JOIN pacientes p ON c.id_paciente = p.id_paciente
+GROUP BY m.nombre, p.nombre
+ORDER BY m.nombre;
+
  

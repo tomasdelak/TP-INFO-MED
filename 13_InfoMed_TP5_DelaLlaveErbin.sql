7813f68ef0533f431f08c08ac209c25e12b68f1a
tomasdelak <tdelallave@itba.edu.ar>
2025-05-26 21:02:16 -0300

    cargo query 13

--- 13_InfoMed_TP5_DelaLlaveErbin.sql
+++ 13_InfoMed_TP5_DelaLlaveErbin.sql
@@ -1,8 +1,10 @@
 -- Actividad 13.
 
-SELECT p.nombre, COUNT(*) AS total_recetas
-FROM recetas r
-JOIN pacientes p ON r.id_paciente = p.id_paciente
+SELECT 
+  p.nombre, 
+  COUNT(r.id_receta) AS total_recetas
+FROM pacientes p
+LEFT JOIN recetas r ON r.id_paciente = p.id_paciente
 GROUP BY p.nombre
 ORDER BY total_recetas DESC;
 
